d1c270a47a906d0b4801c77cc6d06b7107dcfd2e
spuun <kek@spuun.art>
2022-01-30 14:12:48 +0700

    Fully working, demoing.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require("express"),
   fs = require("fs"),
   crypto = require("crypto"),
   pkg = require("./package.json"),
-  Pagination = require("discord-paginationembed"),
+  { Pagination } = require("discordjs-button-embed-pagination"),
   defaultFortunes = fs.existsSync("./utils/fortunes")
     ? fs
         .readFileSync("./utils/fortunes", "utf8")
@@ -35,8 +35,17 @@ function login() {
 }
 
 function isHex(h) {
+  if (typeof h !== "string") return false;
   var a = parseInt(h, 16);
   return a.toString(16) === h.toLowerCase();
+}
+
+function split(arr, n) {
+  let res = [];
+  for (let i = 0; i < arr.length; i += n) {
+    res.push(arr.slice(i, i + n));
+  }
+  return res;
 }
 
 login();
@@ -46,10 +55,16 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
-  if (message.author.bot || !message.content.trim().startsWith(procenv.PREFIX))
+  if (
+    message.author.bot ||
+    !message.content.toLowerCase().trim().startsWith(procenv.BOTPREFIX)
+  )
     return;
-
-  const args = message.content.trim().slice(procenv.PREFIX.length).split(/ +/g),
+  const args = message.content
+      .toLowerCase()
+      .trim()
+      .slice(procenv.BOTPREFIX.length)
+      .split(/ +/g),
     command = args.shift().toLowerCase(),
     subcommands = [
       "add",
@@ -61,15 +76,20 @@ client.on("messageCreate", (message) => {
       "clean",
       "help",
       "info",
+      "unparticipate",
     ];
 
-  if (!command == "fortune") return;
+  if (command != "fortune") return;
 
-  if (!(subcommands.includes(args[0]) && !isHex(args[0]) && args.length > 0))
-    return message.reply(`Invalid subcommand!`);
+  if (args.length > 0) {
+    if (!subcommands.includes(args[0]) && !isHex(args[0]))
+      return message.reply(`Invalid subcommand!`);
+  }
 
   /** @type {Array<Object>} */
   const fortunes = db.ensure("fortunes", defaultFortunes);
+  db.ensure("participated", []);
+  let fortune;
 
   switch (args[0]) {
     case "add":
@@ -81,38 +101,40 @@ client.on("messageCreate", (message) => {
         let id;
         do {
           id = crypto.randomBytes(16).toString("hex").slice(0, 8);
-        } while (db.filterArray("fortunes", (a) => a.id == id).length > 0);
+        } while (db.get("fortunes").filter((a) => a.id == id).length > 0);
         return id;
       })();
 
       // Add the fortune to the database.
       db.push("fortunes", {
         id: fortuneID,
-        fortune: message.content.split(args[0])[1],
+        fortune: message.content.split(args[0])[1].trim().trimEnd(),
         opened: false,
       });
 
-      message.reply({
-        content: `Added fortune with ID: ${fortuneID}!`,
-        allowedMentions: {
-          repliedUser: false,
-        },
-      });
-
-      message.delete();
+      message
+        .reply({
+          content: `Added fortune with ID: ${fortuneID}!`,
+          allowedMentions: {
+            repliedUser: false,
+          },
+        })
+        .then((msg) => {
+          message.delete();
+        });
       break;
 
     case "remove":
       if (!args[1])
         return message.channel.send("You need to provide a fortune ID!");
-      let filtered = db.filterArray("fortunes", (a) => a.id == args[1])[0];
+      let filtered = db.get("fortunes").filter((a) => a.id == args[1])[0];
 
       if (!filtered)
         return message.channel.send("That fortune ID does not exist!");
 
       db.set(
         "fortunes",
-        db.filterArray("fortunes", (a) => a.id != args[1])
+        db.get("fortunes").filter((a) => a.id != args[1])
       );
       message.reply({
         content: `Removed fortune with ID: ${args[1]}!`,
@@ -128,23 +150,16 @@ client.on("messageCreate", (message) => {
 
       let cookies = db.get("fortunes").map((a) => {
         if (a.opened) return "📜";
-        else return "🍪";
+        else return "📦";
       });
 
       let pages = [],
-        rows = [];
+        rows = split(
+          db.get("fortunes").map((a, i) => `${cookies[i]}||(${a.id})||`),
+          3
+        ).map((a) => a.join(" "));
 
-      for (let i = 0; i < db.get("fortunes").length; i + 5) {
-        rows.push(
-          db
-            .get("fortunes")
-            .slice(i, i + 5)
-            .map((a) => `${a.id} : ${a.id}`)
-            .join(" ")
-        );
-      }
-
-      for (let i = 0; i < rows.length; i + 3) {
+      for (let i = 0; i < rows.length; i += 3) {
         let embed = new Discord.MessageEmbed()
           .setTitle("📜 Fortunes")
           .setDescription(
@@ -154,21 +169,21 @@ client.on("messageCreate", (message) => {
           )
           .setColor("#0099ff")
           .setFooter({
-            text: `Page ${i / 3 + 1} of ${Math.ceil(rows.length / 3)}`,
+            text: `Showing ${i ? i * 3 + 1 : 1}-${
+              rows
+                .slice(i, i + 3)
+                .flat(1)
+                .map((e) => e.split(" "))
+                .flat(1).length +
+              i * 3
+            } of ${db.get("fortunes").length} fortunes.`,
           })
           .setTimestamp();
 
         pages.push(embed);
       }
 
-      new Pagination.Embeds()
-        .setArray(pages)
-        .setAuthorizedUsers([message.author.id])
-        .setChannel(message.channel)
-        .setPageIndicator(true)
-        .setPage(1)
-        .setTimeout(15000)
-        .build();
+      new Pagination(message.channel, pages, "Page").paginate();
       break;
 
     case "clear":
@@ -190,7 +205,7 @@ client.on("messageCreate", (message) => {
           let a = db.get("fortunes");
           for (let i = a.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
+            [a[i], a[j]] = [a[j], a[i]];
           }
           return a;
         })()
@@ -204,8 +219,21 @@ client.on("messageCreate", (message) => {
       });
       break;
 
+    case "clean":
+      db.set(
+        "fortunes",
+        db.get("fortunes").filter((a) => !a.opened)
+      );
+      message.reply({
+        content: "Cleaned all closed fortunes!",
+        allowedMentions: {
+          repliedUser: false,
+        },
+      });
+      break;
+
     case "info":
-      let fortune = db.filterArray("fortunes", (a) => a.id == args[1])[0];
+      fortune = db.get("fortunes").filter((a) => a.id == args[1])[0];
       message.reply({
         content: `**Fortune ID:** ${fortune.id}\n**Status:** ${
           fortune.opened ? "Opened" : "Closed"
@@ -216,11 +244,19 @@ client.on("messageCreate", (message) => {
       });
       break;
 
-    case !args[0] || "random":
+    case undefined:
+    case "random":
+      if (db.get("participated").includes(message.author.id))
+        return message.reply(`You've already opened a fortune!`);
+      if (db.get("fortunes").length == 0)
+        return message.channel.send("There are no fortunes!");
+      if (db.get("fortunes").filter((a) => !a.opened).length == 0)
+        return message.channel.send("There are no fortunes left!");
+
       const int = (() => {
         let int;
         do {
-          int = crypto.randomInt(1, fortunes.length);
+          int = crypto.randomInt(0, db.get("fortunes").length);
         } while (db.get("fortunes")[int].opened);
         return int;
       })();
@@ -229,13 +265,36 @@ client.on("messageCreate", (message) => {
       message.author.send(
         `**Here's your fortune!**\n(Fortune ID: ${fortune.id})\n\n${fortune.fortune}`
       );
+
+      db.set(
+        "fortunes",
+        db.get("fortunes").map((a) => {
+          if (a.id == fortune.id) a.opened = true;
+          return a;
+        })
+      );
+
+      db.push("participated", message.author.id);
       break;
 
-    case isHex(args[0]):
-      fortune = db.filterArray("fortunes", (a) => a.id == args[0])[0];
-      if (!fortune) return message.reply("That fortune ID does not exist!");
+    case "unparticipate":
+      if (!args[1])
+        return message.channel.send("You need to provide a user ID!");
+      let user = message.guild.members.cache.get(args[1]);
+
+      if (!user) return message.channel.send("That user ID does not exist!");
+      if (!db.get("participated").includes(user.id))
+        return message.channel.send(
+          "That user hasn't participated in a fortune!"
+        );
+
+      db.set(
+        "participated",
+        db.get("participated").filter((a) => a != user.id)
+      );
+
       message.reply({
-        content: `**Here's your fortune!**\n(Fortune ID: ${fortune.id})\n\n${fortune.fortune}`,
+        content: "User removed from participated list!",
         allowedMentions: {
           repliedUser: false,
         },
@@ -243,12 +302,54 @@ client.on("messageCreate", (message) => {
       break;
 
     case "help":
+      // Generate an embed for it cuz embeds r kooool
+      let embed = new Discord.MessageEmbed()
+        .setTitle("📚 Fortunes Help 📚")
+        .setDescription(
+          `Here are all the commands for the Fortune Bot!\n\n**${procenv.BOTPREFIX}fortune add <fortune>** - Adds a fortune to the database.
+**${procenv.BOTPREFIX}fortune remove <fortuneID>** - Removes a fortune from the database.
+**${procenv.BOTPREFIX}fortune list** - Lists all the fortunes.
+**${procenv.BOTPREFIX}fortune clear** - Clears all the fortunes.
+**${procenv.BOTPREFIX}fortune shuffle** - Shuffles all the fortunes.
+**${procenv.BOTPREFIX}fortune info <fortuneID>** - Shows info about a fortune.
+**${procenv.BOTPREFIX}fortune random or ${procenv.BOTPREFIX}fortune** - Opens a random fortune.
+**${procenv.BOTPREFIX}fortune unparticipate <userID>** - Removes a user from the \`participated\` list
+**${procenv.BOTPREFIX}fortune help** - Shows this help message.`
+        )
+        .setColor("#CC9902")
+        .setFooter({
+          text: "Art Union Fortune Box.",
+        })
+        .setTimestamp();
+
       message.reply({
-        content: `**Fortune Help**\n\n**Fortune**\n\n**Fortune add <fortune>** - Adds a fortune to the database.\n**Fortune remove <ID>** - Removes a fortune from the database.\n**Fortune list** - Lists all fortunes.\n**Fortune clear** - Clears all fortunes.\n**Fortune shuffle** - Shuffles all fortunes.\n**Fortune random** - Gets a random fortune.\n**Fortune info <ID>** - Gets information about a fortune.\n**Fortune help** - Shows this help message.\n\n**Fortune**\n\n**Fortune add <fortune>** - Adds a fortune to the database.\n**Fortune remove <ID>** - Removes a fortune from the database.\n**Fortune list** - Lists all fortunes.\n**Fortune clear** - Clears all fortunes.\n**Fortune shuffle** - Shuffles all fortunes.\n**Fortune random** - Gets a random fortune.\n**Fortune info <ID>** - Gets information about a fortune.\n**Fortune help** - Shows this help message.`,
+        embeds: [embed],
         allowedMentions: {
           repliedUser: false,
         },
       });
       break;
+
+    default:
+      if (isHex(args[0])) {
+        if (db.get("participated").includes(message.author.id))
+          return message.reply(`You've already opened a fortune!`);
+        fortune = db.get("fortunes").filter((a) => a.id == args[0])[0];
+        if (!fortune) return message.reply("That fortune ID does not exist!");
+        if (fortune.opened)
+          return message.reply("That fortune has already been opened!");
+        message.author.send({
+          content: `**Here's your fortune!**\n(Fortune ID: ${fortune.id})\n\n${fortune.fortune}`,
+        });
+
+        db.set(
+          "fortunes",
+          db.get("fortunes").map((a) => {
+            if (a.id == fortune.id) a.opened = true;
+            return a;
+          })
+        );
+        db.push("participated", message.author.id);
+      }
   }
 });
